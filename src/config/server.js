1ff4a775f939b8aa159af1fa34dec9d27197914b
Smooke09 <pedrolucas1014zz@gmail.com>
2022-10-14 10:50:16 -0300

    bug fixed heroku 1/2

--- src/config/server.js
+++ src/config/server.js
@@ -8,7 +8,6 @@ const app = express();
 const apiErrorHandler = require("../error/errorHandler");
 const { Server } = require("socket.io");
 const server = require("http").createServer(app);
-const validationToken = require("../middlewares/validationTokenMIddleware");
 
 const io = new Server(server, {
   cors: {
