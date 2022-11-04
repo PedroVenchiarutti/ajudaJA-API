db7f98f07be66820b5986cb2e5514b9a082724b0
Smooke09 <pedrolucas1014zz@gmail.com>
2022-11-04 08:27:51 -0300

    ajust message error validation

--- src/validations/passwordValidation.js
+++ src/validations/passwordValidation.js
@@ -1,14 +1,14 @@
 const yup = require("yup");
 
 const passwordToExchange = yup.object().shape({
-  password: yup.string().required("Senha atual é obrigatória"),
+  password: yup.string().required({ message: "Senha atual é obrigatória" }),
   newPassword: yup
     .string()
-    .required("A nova senha é obrigatória")
-    .min(6, "A senha deve ter no minimo 6 caracteres"),
-  confirmPassword: yup
-    .string()
-    .oneOf([yup.ref("newPassword"), null], "As senhas nao sao iguais"),
+    .required({ message: "A nova senha é obrigatória" })
+    .min(6, { message: "A senha deve ter no minimo 6 caracteres" }),
+  confirmPassword: yup.string().oneOf([yup.ref("newPassword"), null], {
+    message: "As senhas nao sao iguais",
+  }),
 });
 
 module.exports = passwordToExchange;
