c151ff484cf9db2d4692e7a4e7e9cea92736000e
Smooke09 <pedrolucas1014zz@gmail.com>
2022-11-08 11:29:12 -0300

    chore: swagger bug

--- src/config/server.js
+++ src/config/server.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const allowCors = require("./cors");
 const routes = require("../routes/index");
 const app = express();
-const path = require("path");
 const apiErrorHandler = require("../error/errorHandler");
 const { Server } = require("socket.io");
 const server = require("http").createServer(app);
@@ -20,6 +19,8 @@ const io = new Server(server, {
 // Swagger
 // const swaggerUI = require("swagger-ui-express");
 // const swaggerFile = require("./swagger_output.json");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger_output.json");
 
 app.use(bodyParser.json());
 app.use(
@@ -29,18 +30,17 @@ app.use(
 );
 app.use(allowCors);
 app.use(cors());
-// app.use(express.static(__dirname + "/public"));
 
-app.use(express.static("public"));
-app.use(express.static(__dirname));
+// funciono local
+// app.use(express.static("public"));
+// app.use(express.static(__dirname));
+
+app.use("/public", express.static("public"));
 
 //EndPoint
 app.use("/api", routes);
 // Middleware de tratamento de erros
 app.use(apiErrorHandler);
-
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger_output");
 
 const options = {
   customCssUrl: "/swagger-ui.css",
@@ -48,14 +48,6 @@ const options = {
 };
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
-
-// app.use("/docs", swaggerUi.serve);
-// app.get("/docs", (req, res) => {
-//   // console.log("teste");
-//   res.send(swaggerUi.generateHTML(swaggerDocument, options));
-// });
-
-// app.use("/public/docs", express.static(path.join(SRC_FOLDER, "public")));
 
 // NAO ENVIAR O SWAGGER NA PRODUCAO
 // app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
