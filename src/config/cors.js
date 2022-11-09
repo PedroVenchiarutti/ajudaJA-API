da8f2a26eab258a11e01c388a995f8fd392f20f6
Smooke09 <pedrolucas1014zz@gmail.com>
2022-11-09 11:42:17 -0300

    chore: cors settigns

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
