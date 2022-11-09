f2dfb82cef250c6abf0d0353917e4e9a3fae5848
Smooke09 <pedrolucas1014zz@gmail.com>
2022-11-09 11:52:48 -0300

    chore: cors settigns

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
@@ -6,6 +7,7 @@ module.exports = (req, res, next) => {
   );
   res.header(
     "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
