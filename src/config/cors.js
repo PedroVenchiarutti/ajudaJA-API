a94c5514f96fab7428bcff516491e6cea4efddca
Smooke09 <pedrolucas1014zz@gmail.com>
2022-11-09 13:15:10 -0300

    chore: clean archives of cores

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,56 +1,41 @@
-// module.exports = (req, res, next) => {
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Origin", "*");
-//   // another common pattern
-//   // res.header('Access-Control-Allow-Origin', req.headers.origin);
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-//   );
-//   next();
-// };
-
 module.exports = (req, res, next) => {
-  //set header first to allow request or origin domain (value can be different)
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, OPTIONS, DELETE"
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
   );
-
-  //---- other code
-
-  //Preflight CORS handler
-  if (req.method === "OPTIONS") {
-    console.log(req.method);
-    res.status(200).json({
-      body: "OK",
-    });
-  }
-
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   next();
 };
 
 // module.exports = (req, res, next) => {
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.header(
+//   //set header first to allow request or origin domain (value can be different)
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
 //     "Access-Control-Allow-Headers",
-//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     "Origin, X-Requested-With, Content-Type, Accept"
 //   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, OPTIONS, DELETE"
+//   );
+
+//   //---- other code
+
+//   //Preflight CORS handler
+//   if (req.method === "OPTIONS") {
+//     console.log(req.method);
+//     res.status(200).json({
+//       body: "OK",
+//     });
+//   }
+
 //   next();
 // };
